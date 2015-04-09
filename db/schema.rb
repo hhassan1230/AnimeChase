b8adc2fef275ae5c51264faba07f907c7f928d64
hhassan1230 <hhassan@oswego.edu>
2015-04-09 14:52:33 -0400

    got score working

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150408145917) do
+ActiveRecord::Schema.define(version: 20150409182806) do
 
   create_table "animes", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.string   "img_url"
-    t.integer  "score"
+    t.float    "score"
     t.string   "slug"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
